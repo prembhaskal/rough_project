cc114215451c69a9980ed9ae1ce990c60afba939
prem <premkumar.bhaskal@nokia.com>
2018-11-22 20:30:05 +0530

    jaxb parser for koala stuff.

---
+++ src/main/java/com/xml/KoalaJaxbParser.java
@@ -0,0 +1,50 @@
+package com.xml;
+
+import com.xml.koala.model.Adaptation;
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.UnmarshallerHandler;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.transform.sax.SAXSource;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class KoalaJaxbParser {
+
+    // TODO -- use this instead https://stackoverflow.com/questions/8626153/make-jaxb-go-faster/8626388#8626388
+
+    public static void main(String[] args) throws IOException {
+        JAXBContext jaxbContext = null;
+        BufferedInputStream inputStream = null;
+
+        try {
+            jaxbContext = JAXBContext.newInstance(Adaptation.class);
+            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+            SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
+            SAXParser saxParser = saxParserFactory.newSAXParser();
+            XMLReader xmlReader = saxParser.getXMLReader();
+
+            inputStream = new BufferedInputStream(new FileInputStream("d://tmp/glsp/pmcbisKOALA.xml"));
+            InputSource inputSource = new InputSource(inputStream);
+            SAXSource saxSource = new SAXSource(xmlReader, inputSource);
+
+            JAXBElement<Adaptation> unmarshal = unmarshaller.unmarshal(saxSource, Adaptation.class);
+            Adaptation value = unmarshal.getValue();
+            System.out.println("done");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+    }
+}
